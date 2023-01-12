5cb90f02a5b452c4e8beb7a795a447178a35abb8
prasad-kumkar <pk111_@live.com>
2023-01-12 14:02:47 +0530

    initialized

--- scripts/initiate.ts
+++ scripts/initiate.ts
@@ -2,81 +2,103 @@ import hre, { ethers, upgrades } from "hardhat";
 import { Contract } from 'ethers';
 
 export async function initiate(synthex: Contract, oracle: Contract, deployments: any, config: any) {
-  const MockToken = await ethers.getContractFactory("MockToken");
-  const ERC20X = await ethers.getContractFactory("ERC20X");
-  const PriceFeed = await ethers.getContractFactory("MockPriceFeed");
-
+  
   console.log("\nDeploying Collaterals... 💬");
+
   for(let i in config.collaterals){
     let collateral = config.collaterals[i].address;
     if(!collateral){
       // deploy collateral token
-      const token = await MockToken.deploy(config.collaterals[i].name, config.collaterals[i].symbol);
+      const token = await _deploy('MockToken', [config.collaterals[i].name, config.collaterals[i].symbol], deployments, {name: config.collaterals[i].symbol});
       collateral = token.address;
-      deployments.contracts[config.collaterals[i].symbol] = {
-        address: collateral,
-        source: "MockToken",
-        constructorArguments: [config.collaterals[i].name, config.collaterals[i].symbol]
-      };
-      deployments.sources["MockToken"] = MockToken.interface.format("json")
     }
     let feed = config.collaterals[i].feed;
     if(!feed){
       // deploy price feed
-      const priceFeed = await PriceFeed.deploy(ethers.utils.parseUnits(config.collaterals[i].price, 8), 8);
+      const priceFeed = await _deploy('MockPriceFeed', [ethers.utils.parseUnits(config.collaterals[i].price, 8), 8], deployments, {name: `${config.collaterals[i].symbol}_PriceFeed`});
       await oracle.setFeed(collateral, priceFeed.address);
       feed = priceFeed.address;
     }
     await synthex.enableCollateral(collateral, ethers.utils.parseEther(config.collaterals[i].volatilityRatio));
-    console.log(`\t Collateral ${config.collaterals[i].symbol} deployed`);
+    console.log(`\t Collateral ${config.collaterals[i].symbol} deployed successfully ✅`);
   }
+  console.log("Collaterals deployed successfully 🎉 \n");
 
-  console.log("Collaterals deployed successfully ✅ \n");
-  const SyntheXPool = await ethers.getContractFactory("SyntheXPool");
   console.log("Deploying Trading Pools... 💬");
-
   for(let i in config.tradingPools){
     // deploy pools
-    const pool = await upgrades.deployProxy(SyntheXPool, [config.tradingPools[i].name, config.tradingPools[i].symbol, synthex.address]);
+    const pool = await _deploy('SyntheXPool', [config.tradingPools[i].name, config.tradingPools[i].symbol, synthex.address], deployments, {name: config.tradingPools[i].symbol, upgradable: true});
     // enable trading pool
     await synthex.enableTradingPool(pool.address, ethers.utils.parseEther(config.tradingPools[i].volatilityRatio))
     // set reward speed
     await synthex.setPoolSpeed(pool.address, ethers.utils.parseEther(config.tradingPools[i].rewardSpeed));
     // set fee
     await pool.updateFee(ethers.utils.parseEther(config.tradingPools[i].fee));
-    // add to deployments
-    deployments.contracts[config.tradingPools[i].symbol] = {
-      address: pool.address,
-      source: "SyntheXPool",
-      constructorArguments: [] // not needed for verification
-    };
-    deployments.sources["SyntheXPool"] = SyntheXPool.interface.format("json")
-    console.log(`\t Trading Pool ${config.tradingPools[i].symbol} deployed`)
+
+    console.log(`\t Trading Pool ${config.tradingPools[i].symbol} deployed successfully ✅`);
 
     for(let j in config.tradingPools[i].synths){
       let synth = config.tradingPools[i].synths[j].address;
       if(!synth){
         // deploy token
-        const token = await ERC20X.deploy(config.tradingPools[i].synths[j].name, config.tradingPools[i].synths[j].symbol, pool.address);
+        const token = await _deploy('ERC20X', [config.tradingPools[i].synths[j].name, config.tradingPools[i].synths[j].symbol, pool.address], deployments, { name: config.tradingPools[i].synths[j].symbol });
         synth = token.address;
-        deployments.contracts[config.tradingPools[i].synths[j].symbol] = {
-          address: synth,
-          source: "ERC20X",
-          constructorArguments: [config.tradingPools[i].synths[j].name, config.tradingPools[i].synths[j].symbol, pool.address]
-        };
-        deployments.sources["ERC20X"] = ERC20X.interface.format("json")
       }
       let feed =  config.tradingPools[i].synths[j].feed;
       if(!feed){
         // deploy price feed
-        const priceFeed = await PriceFeed.deploy(ethers.utils.parseUnits(config.tradingPools[i].synths[j].price, 8), 8);
+        const priceFeed = await _deploy('MockPriceFeed', [ethers.utils.parseUnits(config.tradingPools[i].synths[j].price, 8), 8], deployments, {name: `${config.tradingPools[i].synths[j].symbol}_PriceFeed`});
         await oracle.setFeed(synth, priceFeed.address);
         feed = priceFeed.address;
       }
       await pool.enableSynth(synth);
-      console.log(`\t\t Synth ${config.tradingPools[i].synths[j].symbol} added`);
+      console.log(`\t\t Synth ${config.tradingPools[i].synths[j].symbol} added to ${config.tradingPools[i].symbol} ✨`);
     }
   }
   
-  console.log("Trading Pools deployed successfully ✅\n");
+  console.log("Trading Pools deployed successfully 🎉\n");
 }
+
+const _deploy = async (
+	contractName: string,
+	args: any[],
+	deployments: any,
+	{upgradable = false, name = contractName} = {},
+	config: any = {},
+) => {
+	const Contract = await ethers.getContractFactory(contractName);
+	let contract;
+	if (upgradable) {
+		contract = await upgrades.deployProxy(Contract, args, { type: 'uups' });
+		args = [];
+	} else {
+		contract = await Contract.deploy(...args);
+	}
+	await contract.deployed();
+
+	deployments.contracts[name] = {
+		address: contract.address,
+		abi: contractName,
+		constructorArguments: args,
+		block: (await ethers.provider.getBlockNumber()).toString(),
+	};
+	deployments.sources[contractName] = JSON.parse(
+		Contract.interface.format("json") as string
+	);
+
+	if (upgradable) {
+		const implementationAddress = await upgrades.erc1967.getImplementationAddress(contract.address);
+		if(!deployments.contracts[name].implementations) deployments.contracts[name].implementations = {};
+		deployments.contracts[name].implementations[config.latest] = {
+			address: implementationAddress,
+			source: name+'_'+config.latest,
+			constructorArguments: [],
+			version: config.latest,
+			block: (await ethers.provider.getBlockNumber()).toString()
+		};
+		deployments.contracts[name].latest = implementationAddress;
+		deployments.sources[name+'_'+config.latest] = contract.interface.format('json');
+	}
+
+	return contract;
+};
