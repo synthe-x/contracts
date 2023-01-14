d257445f59737c47cdf0c50a4ac640c91c1ae616
Prajakta Thomas <prajakta.k2206@gmail.com>
2023-01-14 15:55:47 +0530

    Stakning rewards

---
+++ contracts/interfaces/IStaking.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IStaking {
+
+    function stake(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function exit() external;
+
+    function getReward() external;
+
+    //View
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function earned(address account) external view returns (uint256);
+
+    function getRewardForDuration() external view returns (uint256);
+
+    function lastTimeRewardApplicable() external view returns (uint256);
+
+    function rewardPerToken() external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    /* ========== EVENTS ========== */
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event RewardsDurationUpdated(uint256 newDuration);
+    event Recovered(address token, uint256 amount);
+
+
+}
