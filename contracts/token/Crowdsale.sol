d38c42189ff3b22e64c28647fb0f70dd514bbfae
prasad-kumkar <pk111_@live.com>
2023-01-15 09:32:57 +0530

    tested staking

---
+++ contracts/token/Crowdsale.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Crowdsale contract that allows users to buy SYN tokens with ETH
+// Issued tokens are released after 180 days
+contract Crowdsale {
+    
+}
---
+++ test/staking.test.ts
@@ -0,0 +1,86 @@
+// import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import deploy from "../scripts/test";
+import { ETH_ADDRESS } from "../scripts/utils/const";
+
+describe("Testing Fee", function () {
+	let stakedSyn: any, syn: any, stakingRewards: any;
+	let owner: any, user1: any, user2: any, user3: any;
+
+	before(async () => {
+		// Contracts are deployed using the first signer/account by default
+		[owner, user1, user2, user3] = await ethers.getSigners();
+
+		const deployments = await deploy(owner.address);
+		syn = deployments.syn;
+		stakedSyn = deployments.sealedSYN;
+		stakingRewards = deployments.stakingRewards;
+
+		// for user1 to stake
+		await stakedSyn.mint(user1.address, ethers.utils.parseEther("10000"));
+        // for user2 to stake
+		await stakedSyn.mint(user2.address, ethers.utils.parseEther("10000"));
+		// for owner to add reward
+		await syn.mint(owner.address, ethers.utils.parseEther("10000"));
+	});
+
+	it("add 1000 syn reward for 1 year", async function () {
+		await syn
+			.connect(owner)
+			.approve(stakingRewards.address, ethers.utils.parseEther("1000"));
+		await stakingRewards
+			.connect(owner)
+			.setRewardsDuration(ethers.BigNumber.from("31536000"));
+		await stakingRewards
+			.connect(owner)
+			.addReward(ethers.utils.parseEther("1000"));
+		expect(await stakingRewards.rewardRate()).to.equal(
+			ethers.utils
+				.parseEther("1000")
+				.div(ethers.BigNumber.from("31536000"))
+		);
+	});
+
+	it("user1 should stake 1000 syn", async function () {
+		await stakedSyn
+			.connect(user1)
+			.approve(stakingRewards.address, ethers.utils.parseEther("1000"));
+		await stakingRewards
+			.connect(user1)
+			.stake(ethers.utils.parseEther("1000"));
+		expect(await stakingRewards.balanceOf(user1.address)).to.equal(
+			ethers.utils.parseEther("1000")
+		);
+	});
+
+	it("view reward APY should be 100%", async function () {
+		// TODO calculate APY
+
+		// _totalSupply
+		expect(await stakingRewards.totalSupply()).to.equal(
+			ethers.utils.parseEther("1000")
+		);
+	});
+
+    it("user2 should stake 500 syn", async function () {
+		await stakedSyn
+			.connect(user2)
+			.approve(stakingRewards.address, ethers.utils.parseEther("500"));
+		await stakingRewards
+			.connect(user2)
+			.stake(ethers.utils.parseEther("500"));
+		expect(await stakingRewards.balanceOf(user2.address)).to.equal(
+			ethers.utils.parseEther("500")
+		);
+	});
+
+	it("view reward APY 66%", async function () {
+       	// TODO calculate APY
+
+		// _totalSupply
+		expect(await stakingRewards.totalSupply()).to.equal(
+			ethers.utils.parseEther("1500")
+		);
+	});
+});
