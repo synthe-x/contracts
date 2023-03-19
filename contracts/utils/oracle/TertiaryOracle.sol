52853068c61a0bce4627744429f975202c008b20
prasad-kumkar <pk111_@live.com>
2023-03-19 16:08:15 +0530

    0.16.0

--- contracts/utils/oracle/TertiaryOracle.sol
+++ contracts/utils/oracle/TertiaryOracle.sol
@@ -2,27 +2,31 @@
 pragma solidity ^0.8.0;
 
 import "@aave/core-v3/contracts/dependencies/chainlink/AggregatorInterface.sol";
+// Safemath int
 import "@openzeppelin/contracts/utils/math/SignedSafeMath.sol";
 
 /**
- * @title Secondary oracle 
+ * @title TertiaryOracle
  * @notice This contract is used to get the price of a token (in usd) derived from the price of another token (in usd)
- * @notice Eg. COMP/USD, from COMP/ETH (18) and ETH/USD (8)
+ * @notice Eg. wstETH/USD, from wstETH/stETH (18), stETH/ETH (18), ETH/USD (8)
  */
-contract SecondaryOracle {
+contract TertiaryOracle {
     using SignedSafeMath for int;
-    // COMP/ETH oracle
+    // wstETH/stETH oracle
     address PRIMARY_ORACLE;
-    // ETH/USD oracle
+    // stETH/ETH oracle
     address SECONDARY_ORACLE;
+    // stETH/ETH oracle
+    address TERTIARY_ORACLE;
 
-    constructor(address _primaryOracle, address _secondaryOracle) {
+    constructor(address _primaryOracle, address _secondaryOracle, address _tertiaryOracle) {
         PRIMARY_ORACLE = _primaryOracle;
         SECONDARY_ORACLE = _secondaryOracle;
+        TERTIARY_ORACLE = _tertiaryOracle;
     }
 
     function latestAnswer() external view returns (int256) {
-        return AggregatorInterface(PRIMARY_ORACLE).latestAnswer().mul(AggregatorInterface(SECONDARY_ORACLE).latestAnswer()).div(10**18);
+        return AggregatorInterface(PRIMARY_ORACLE).latestAnswer().mul(AggregatorInterface(SECONDARY_ORACLE).latestAnswer()).div(10**18).mul(AggregatorInterface(TERTIARY_ORACLE).latestAnswer()).div(10**18);
     }
 
     function decimals() external pure returns (uint8) {
