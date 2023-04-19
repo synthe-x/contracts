e32084b85d82a593d082a32856bc8ddcbbc88662
prasad-kumkar <pk111_@live.com>
2023-04-19 20:13:09 +0530

    swap fee in given out

--- test/swaps/givenOut.test.ts
+++ test/swaps/givenOut.test.ts
@@ -8,7 +8,7 @@ describe("Testing Swaps: Given Out", function () {
 	let synthex: any, oracle: any, pool: any, eth: any, susd: any, sbtc: any, seth: any, sbtcFeed: any;
 	let owner: any, user1: any;
 
-	before(async () => {
+	beforeEach(async () => {
 		// Contracts are deployed using the first signer/account by default
 		[owner, user1] = await ethers.getSigners();
 
@@ -42,4 +42,19 @@ describe("Testing Swaps: Given Out", function () {
 		const user1Liquidity = await pool.getAccountLiquidity(user1.address);
 		expect(user1Liquidity[2]).to.be.equal(ethers.utils.parseEther("10000.00"));
     })
+
+	it("with 1% fee", async () => {
+		await pool.connect(owner).updateSynth(sbtc.address, {mintFee: 100, isActive: true, isDisabled: false, burnFee: 0});
+
+		// user1 exchanges 10 seth for 1 sbtc
+		await expect(pool.connect(user1).swap(seth.address, ethers.utils.parseEther("1"), sbtc.address, 1, user1.address)).to.be.revertedWith("ERC20: burn amount exceeds balance");
+
+		// user1 exchanges 1 seth for 0.1 sbtc
+		await pool.connect(user1).swap(seth.address, ethers.utils.parseEther("0.1"), sbtc.address, 1, user1.address);
+
+		// check balances
+		let sethAmountExpectedToSpend = ethers.utils.parseEther("1").mul(101).div(100);
+		expect(await seth.balanceOf(user1.address)).to.closeTo(ethers.utils.parseEther("10").sub(sethAmountExpectedToSpend), ethers.utils.parseEther('0.001'));
+		expect(await sbtc.balanceOf(user1.address)).to.equal(ethers.utils.parseEther("0.1"));
+	})
 });
